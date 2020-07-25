df3b7deaad31d025cfe6a1d073bc797284853df0
returntears <returntears@outlook.com>
2020-07-26 00:10:42 +0800

    自定义LoadBalance负载均衡算法:手写轮询算法中的little problem 修改

--- allst-cloud-order/src/main/java/com/allst/springcloud/lb/MyLB.java
+++ allst-cloud-order/src/main/java/com/allst/springcloud/lb/MyLB.java
@@ -14,7 +14,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 @Component
 public class MyLB implements LoadBalancer {
 
-    private AtomicInteger atomicInteger = new AtomicInteger(0);
+    private final AtomicInteger atomicInteger = new AtomicInteger(0);
 
     public final int getAndIncrement() {
         int current;
@@ -23,7 +23,7 @@ public class MyLB implements LoadBalancer {
         do {
             current = this.atomicInteger.get();
             next = current >= Integer.MAX_VALUE ? 0 : current + 1;
-        } while (this.atomicInteger.compareAndSet(current, next));
+        } while (!this.atomicInteger.compareAndSet(current, next));
         System.out.println("*****第几次访问，次数next: " + next);
         return next;
     }
