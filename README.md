8bd34d9f261bbf66bf25e52f5b004cfdf66adcc3
returntears <returntears@outlook.com>
2020-07-28 00:08:00 +0800

    添加HystrixDashboard配置，并且登录Web可视化界面查看服务状态

--- README.md
+++ README.md
@@ -260,7 +260,9 @@ Hystrix 重要概述:
 Hystrix还提供了准实时的调用监控（Hystrix Dashboard）,Hystrix会持续地记录所有通过Hystrix发起的请求的执行信息。
 并以统计报表和图标的形式展示给用户，包括每秒执行多少请求多少成功，多少失败等， Netflix通过Hystrix-metrics-event-stream项目
 实现了对以上指标的监控。Spring Cloud也提供了Hystrix Dash Board的整合，对监控内容转化成可视化界面。
-
-
+Success | Short-Circuited | Bad Request | Timeout | Rejected | Failure | Error %
+7色 
+http://localhost:9001/hystrix
+http://localhost:8001/actuator/hystrix.stream
 
 ```
--- allst-cloud-hystrix/src/main/java/com/allst/springcloud/PaymentHystrixMain.java
+++ allst-cloud-hystrix/src/main/java/com/allst/springcloud/PaymentHystrixMain.java
@@ -1,9 +1,12 @@
 package com.allst.springcloud;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 /**
  * hystrix主启动类
@@ -16,5 +19,20 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 public class PaymentHystrixMain {
     public static void main(String[] args) {
         SpringApplication.run(PaymentHystrixMain.class, args);
+    }
+
+    /**
+     * 此配置为了服务监控而配置，与服务容错本身无关，Spring Cloud升级后的坑
+     * ServletRegistrationBean因为springboot的默认路径不是/hystrix.stream
+     * 只要在自己的项目中配置上下面的servlet即可
+     */
+    @Bean
+    public ServletRegistrationBean getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/actuator/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
     }
 }
